7060d8ac18f262785857f890cbf2140489cbf75a
Fernando Belmonte Sanches <ferbs89@gmail.com>
2023-03-14 18:03:59 -0300

    Customizing transition presets

--- src/routes/drawer.tsx
+++ src/routes/drawer.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 
 import Container from "../components/Container";
 import Loader from "../components/Loader";
@@ -14,6 +18,7 @@ import PaymentsForm from "../pages/PaymentsForm";
 import Todos from "../pages/Todos";
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function DrawerNavigator() {
   const { user, loadingSession } = useAuth();
@@ -26,6 +31,21 @@ export default function DrawerNavigator() {
     );
   }
 
+  function PaymentsStack() {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          presentation: "transparentModal",
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      >
+        <Stack.Screen name="Payments" component={Payments} />
+        <Stack.Screen name="PaymentsForm" component={PaymentsForm} />
+      </Stack.Navigator>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Drawer.Navigator
@@ -33,13 +53,14 @@ export default function DrawerNavigator() {
         screenOptions={{ headerShown: false }}
       >
         {user ? (
-          <>
-            <Drawer.Screen name="Payments" component={Payments} />
-            <Drawer.Screen name="PaymentsForm" component={PaymentsForm} />
+          <Drawer.Group>
+            <Drawer.Screen name="PaymentsStack" component={PaymentsStack} />
             <Drawer.Screen name="Todos" component={Todos} />
-          </>
+          </Drawer.Group>
         ) : (
-          <Drawer.Screen name="Login" component={Login} />
+          <Drawer.Group>
+            <Drawer.Screen name="Login" component={Login} />
+          </Drawer.Group>
         )}
       </Drawer.Navigator>
     </NavigationContainer>
