ec77898957a1da6e9acc18c0e0887473d505d977
Fernando Belmonte Sanches <ferbs89@gmail.com>
2023-06-30 16:32:41 -0300

    Adjusting icon and splash screen

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Box, Spinner } from "native-base";
+import { Box, Image, Spinner } from "native-base";
+
+const logo = require("../../assets/logo.png");
 
 export default function () {
   return (
     <Box flex="1" alignItems="center" justifyContent="center">
+      <Image source={logo} alt="Lunaris" width={32} height={32} mb="8" />
       <Spinner size="lg" />
     </Box>
   );
